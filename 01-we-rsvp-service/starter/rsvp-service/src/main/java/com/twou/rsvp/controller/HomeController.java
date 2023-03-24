b7afcdd8c3b15f2ad6a809a1171c90b43d29dcb9
bryanmax9 <brillant16.gcv@gmail.com>
2023-03-24 13:34:28 -0700

    changes in Controller

---
+++ 01-we-rsvp-service/starter/rsvp-service/src/main/java/com/twou/rsvp/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.twou.rsvp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping(value = "/")
+    public String index(){
+        return "";
+    }
+}
